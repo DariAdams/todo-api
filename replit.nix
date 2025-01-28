a261bd127c2442a72fe8e5526d4ed239bec7b5ff
Adam Dari <161232329+DariAdams@users.noreply.github.com>
2025-01-28 22:21:18 +0200

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+    deps = [
+      pkgs.lsof
+        pkgs.graalvm17-ce
+        pkgs.maven
+        pkgs.replitPackages.jdt-language-server
+        pkgs.replitPackages.java-debug
+    ];
+}
